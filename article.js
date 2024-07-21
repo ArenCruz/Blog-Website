1c5e07fc107b381e73141c65ac9fcc72f386371a
ArenCruz <aldrinlcruz@outlook.com>
2024-07-22 02:11:56 +0800

    Update article.js
    - Modified article.js to fix the navigation bug (again)

--- article.js
+++ article.js
@@ -1,11 +1,17 @@
 
+const home = document.getElementById('home');
+const about = document.getElementById('about');
+const posts = document.getElementById('posts');
+about.addEventListener('click', () => {window.location.href="index.html"})
+home.addEventListener('click', () => {window.location.href="index.html"})
+posts.addEventListener('click', () => {window.location.href="index.html"})
 
-const navBarAgain = document.getElementsByClassName('navBar')[0];
+const navBar = document.getElementsByClassName('navBar')[0];
 window.addEventListener('scroll', () => {
     if(window.scrollY > 90){
-        navBarAgain.classList.add('navBar-scrolled');
+        navBar.classList.add('navBar-scrolled');
     }
     else if(window.scrollY <=100){
-        navBarAgain.classList.remove('navBar-scrolled');
+        navBar.classList.remove('navBar-scrolled');
     }
 })
